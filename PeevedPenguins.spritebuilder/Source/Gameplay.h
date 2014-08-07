11d03eeafd7a5f1c01cb249b063a1ef7dc4a0cf0
ChelseaXu <ChelseaXu@Chelseas-MacBook-Pro.local>
2014-08-07 13:24:46 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Chelsea Xu on 2014-08-07.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
