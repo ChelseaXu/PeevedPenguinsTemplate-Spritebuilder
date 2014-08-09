a67490efa912cd3370ac658a1b98df1b3cd4c721
ChelseaXu <ChelseaXu@Chelseas-MacBook-Pro.local>
2014-08-08 22:20:28 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Chelsea Xu on 2014-08-08.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
